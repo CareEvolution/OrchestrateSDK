8bb4d899db58f7fb73cb9273916dca9c396bdea5
Jeremy Fortune <jeremy@careevolution.com>
2024-02-27 09:50:11 -0500

    Make Test Environment Setup More Obvious
    Related: https://github.com/CareEvolution/OrchestrateSDK/pull/94/files#r1503271649

--- typescript/tests/httpHandler.test.ts
+++ typescript/tests/httpHandler.test.ts
@@ -1,33 +1,82 @@
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeAll, afterAll } from "vitest";
 import { HttpHandler } from "../src/httpHandler";
 import { createHttpHandler } from "../src/httpHandlerFactory";
 
 vi.mock("../src/httpHandler");
 
-describe("createHttpHandler", () => {
-  it("should create an HttpHandler defaulted to api.careevolutionapi.com", () => {
+describe("createHttpHandler full environment", () => {
+  beforeAll(() => {
+    vi.stubEnv("ORCHESTRATE_BASE_URL", "https://env.example.com");
+    vi.stubEnv("ORCHESTRATE_API_KEY", "env-api-key");
+    vi.stubEnv("ORCHESTRATE_ADDITIONAL_HEADERS", JSON.stringify({ "x-custom-header": "custom-value" }));
+  });
 
+  it("should prefer the environment variables", () => {
+    createHttpHandler(undefined);
+
+    expect(HttpHandler).toHaveBeenCalledWith("https://env.example.com", {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "x-api-key": "env-api-key",
+      "x-custom-header": "custom-value",
+    });
+  });
+
+  it("should prefer the api key parameter over the environment variable", () => {
+    createHttpHandler("my-api-key");
+
+    expect(HttpHandler).toHaveBeenCalledWith("https://env.example.com", {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "x-api-key": "my-api-key",
+      "x-custom-header": "custom-value",
+    });
+  });
+
+  afterAll(() => {
+    vi.unstubAllEnvs();
+  });
+});
+
+describe("createHttpHandler apiKey environment", () => {
+  beforeAll(() => {
+    vi.stubEnv("ORCHESTRATE_API_KEY", "env-api-key");
+  });
+
+  it("should create an HttpHandler with the provided apiKey", () => {
+    createHttpHandler(undefined);
+
+    expect(HttpHandler).toHaveBeenCalledWith("https://api.careevolutionapi.com", {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "x-api-key": "env-api-key",
+    });
+  });
+
+  it("should prefer the api key parameter over the environment variable", () => {
+    createHttpHandler("my-api-key");
+
+    expect(HttpHandler).toHaveBeenCalledWith("https://api.careevolutionapi.com", {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      "x-api-key": "my-api-key",
+    });
+  });
+
+  afterAll(() => {
+    vi.unstubAllEnvs();
+  });
+});
+
+
+describe("createHttpHandler no environment", () => {
+  it("should create an HttpHandler defaulted to api.careevolutionapi.com", () => {
     createHttpHandler(undefined);
 
     expect(HttpHandler).toHaveBeenCalledWith("https://api.careevolutionapi.com", {
       "Content-Type": "application/json",
       Accept: "application/json",
     });
-  });
-
-  it("should prefer the the environment variable", () => {
-    try {
-      vi.stubEnv("ORCHESTRATE_BASE_URL", "https://env.example.com");
-
-      createHttpHandler(undefined);
-
-      expect(HttpHandler).toHaveBeenCalledWith("https://env.example.com", {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      });
-    } finally {
-      vi.unstubAllEnvs();
-    }
   });
 
   it("should create an HttpHandler with the provided apiKey", () => {
@@ -38,53 +87,5 @@ describe("createHttpHandler", () => {
       Accept: "application/json",
       "x-api-key": "my-api-key",
     });
-  });
-
-  it("should prefer the api key parameter over the environment variable", () => {
-    try {
-      vi.stubEnv("ORCHESTRATE_API_KEY", "env-api-key");
-
-      createHttpHandler("my-api-key");
-
-      expect(HttpHandler).toHaveBeenCalledWith("https://api.careevolutionapi.com", {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "x-api-key": "my-api-key",
-      });
-    } finally {
-      vi.unstubAllEnvs();
-    }
-  });
-
-  it("should use api key environment variable if no parameter is provided", () => {
-    try {
-      vi.stubEnv("ORCHESTRATE_API_KEY", "env-api-key");
-
-      createHttpHandler(undefined);
-
-      expect(HttpHandler).toHaveBeenCalledWith("https://api.careevolutionapi.com", {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "x-api-key": "env-api-key",
-      });
-    } finally {
-      vi.unstubAllEnvs();
-    }
-  });
-
-  it("should create an HttpHandler with the provided additional headers", () => {
-    try {
-      vi.stubEnv("ORCHESTRATE_ADDITIONAL_HEADERS", JSON.stringify({ "x-custom-header": "custom-value" }));
-
-      createHttpHandler(undefined);
-
-      expect(HttpHandler).toHaveBeenCalledWith("https://api.careevolutionapi.com", {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        "x-custom-header": "custom-value",
-      });
-    } finally {
-      vi.unstubAllEnvs();
-    }
   });
 });
