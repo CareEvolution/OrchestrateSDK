9a8420eb7039b97d96a57530833109b001184045
Jeremy Fortune <jeremy@careevolution.com>
2024-04-30 11:23:00 -0400

    Reduce Non-E2E Test Timeouts
    Related: https://github.com/CareEvolution/OrchestrateSDK/pull/131#discussion_r1585017458

--- typescript/vitest.config.js
+++ typescript/vitest.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    testTimeout: 300000,
-    hookTimeout: 300000,
     exclude: ["tests/identity/*.test.ts", "node_modules/**/*"]
   },
 });
