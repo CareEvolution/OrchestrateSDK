9f911b0d47650cfa91ac09a3f7cafb095a44283f
Jeremy Fortune <jeremy@careevolution.com>
2025-09-12 10:28:59 -0400

    Expose HTTP Handler Half-Publicly

--- typescript/src/api.ts
+++ typescript/src/api.ts
@@ -1,4 +1,5 @@
 import { ConvertApi } from "./convert.js";
+import { IHttpHandler } from "./httpHandler.js";
 import { createHttpHandler } from "./httpHandlerFactory.js";
 import { InsightApi } from "./insight.js";
 import { TerminologyApi } from "./terminology.js";
@@ -9,9 +10,17 @@ export interface Configuration {
 }
 
 export class OrchestrateApi {
-  terminology: TerminologyApi;
-  convert: ConvertApi;
-  insight: InsightApi;
+  readonly terminology: TerminologyApi;
+  readonly convert: ConvertApi;
+  readonly insight: InsightApi;
+
+  /**
+   * Exposes the underlying IHttpHandler instance for advanced usage. This is
+   * made available to take advantage of features not yet wrapped in
+   * OrchestrateApi. This may change without warning.
+   * @deprecated Not a stable API.
+   */
+  readonly httpHandler: IHttpHandler;
 
   constructor(configuration: Configuration | undefined = {}) {
     const httpHandler = createHttpHandler(configuration.apiKey, undefined, configuration.timeoutMs);
@@ -19,5 +28,7 @@ export class OrchestrateApi {
     this.terminology = new TerminologyApi(httpHandler);
     this.convert = new ConvertApi(httpHandler);
     this.insight = new InsightApi(httpHandler);
+
+    this.httpHandler = httpHandler;
   }
 }
